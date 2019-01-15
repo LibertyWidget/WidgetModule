ea4e3f9c4aee7ad27a920930f9ad6b8a38bf9f70
wangzusong <wangzusong@juwang.cn>
2019-01-15 12:57:36 +0800

    Android 加入系统工具

--- app/src/main/java/com/util/SystemUtils.java
+++ app/src/main/java/com/util/SystemUtils.java
@@ -1,13 +1,25 @@
 package com.util;
 
+import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.Dialog;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Build;
 import android.text.TextUtils;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.util.widget.BuildConfig;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SystemUtils {
@@ -76,5 +88,124 @@ public class SystemUtils {
             }
         }
         return isAppRunning;
+    }
+
+    /**
+     * 设置状态栏的颜色
+     */
+    public static void setWindowStatusBarColor(Activity activity, int colorResId) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = activity.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(activity.getResources().getColor(colorResId));
+                //底部导航栏
+                //window.setNavigationBarColor(activity.getResources().getColor(colorResId));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 设置Dialog状态颜色
+     */
+    public static void setWindowStatusBarColor(Dialog dialog, int colorResId) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = dialog.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(dialog.getContext().getResources().getColor(colorResId));
+
+                //底部导航栏
+                //window.setNavigationBarColor(activity.getResources().getColor(colorResId));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取包名
+     */
+    public static String getPackageName(Context context) {
+        return context.getPackageName();
+    }
+
+    /**
+     * 获取VersionName(版本名称)
+     */
+    public static String getVersionName(Context context) {
+        PackageManager packageManager = getPackageManager(context);
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(context), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 获取VersionCode(版本号)
+     */
+    public static int getVersionCode(Context context) {
+        PackageManager packageManager = getPackageManager(context);
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(context), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * 获取所有安装的应用程序,不包含系统应用
+     */
+    public static List<PackageInfo> getInstalledPackages(Context context) {
+        PackageManager packageManager = getPackageManager(context);
+        List<PackageInfo> packageInfos = packageManager.getInstalledPackages(0);
+        List<PackageInfo> packageInfoList = new ArrayList<>();
+        for (int i = 0; i < packageInfos.size(); i++) {
+            if ((packageInfos.get(i).applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                packageInfoList.add(packageInfos.get(i));
+            }
+        }
+        return packageInfoList;
+    }
+
+    /**
+     * 获取应用程序的icon图标
+     *
+     * @return 当包名错误时，返回null
+     */
+    public static Drawable getApplicationIcon(Context context) {
+        PackageManager packageManager = getPackageManager(context);
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(context), 0);
+            return packageInfo.applicationInfo.loadIcon(packageManager);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 启动安装应用程序
+     *
+     * @param path 应用程序路径
+     */
+    public static void installApk(Activity activity, String path) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(new File(path)), "application/vnd.android.package-archive");
+        activity.startActivity(intent);
+    }
+
+    /**
+     * 获取PackageManager对象
+     */
+    private static PackageManager getPackageManager(Context context) {
+        return context.getPackageManager();
     }
 }
